197cabc8e5f11063e1bc32112d00a32b9fe4c7ac
ThangNguyen <nmthang3@cmc.com.vn>
2021-12-21 16:04:01 +0700

    init login app

---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2A2VjZw_7077PVYa92jabi5pg0QwcfBM",
+    authDomain: "check-list-app-9b6de.firebaseapp.com",
+    databaseURL: "https://check-list-app-9b6de-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "check-list-app-9b6de",
+    storageBucket: "check-list-app-9b6de.appspot.com",
+    messagingSenderId: "815466700910",
+    appId: "1:815466700910:web:18633ab768acbc00db467f",
+    measurementId: "G-4SK2J2BSY7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
